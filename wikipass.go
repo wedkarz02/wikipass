a8e320a364190abc074fee83b04d1e37e3623f4c
Paweł Rybak <pawel.rybak13@gmail.com>
2023-05-16 21:17:09 +0200

    Tested the decryption function

--- wikipass.go
+++ wikipass.go
@@ -1,9 +1,14 @@
 package main
 
 import (
+	"fmt"
 	"wikipass/src"
 )
 
 func main() {
-	src.EncryptAES([]byte("Hello, world!"), []byte("#secret-that-has-to-be-32-bytes!"))
+	cipher := src.EncryptAES([]byte("Hello, world!"), []byte("#secret-that-has-to-be-32-bytes!"))
+	fmt.Println(string(cipher))
+
+	plain := src.DecryptAES(cipher, []byte("#secret-that-has-to-be-32-bytes!"))
+	fmt.Println(string(plain))
 }
