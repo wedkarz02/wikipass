d2f9ccd0712a66f085e01e5588cac7c810705afd
Paweł Rybak <pawel.rybak13@gmail.com>
2023-05-23 22:42:51 +0200

    Cleaned up wiki module

--- pkg/wiki/wiki.go
+++ pkg/wiki/wiki.go
@@ -1,60 +1,32 @@
 package wiki
 
 import (
-
-	// https://www.mediawiki.org/wiki/API:Main_page
-
 	"fmt"
+	"log"
 
 	"cgt.name/pkg/go-mwclient"
 	"cgt.name/pkg/go-mwclient/params"
 )
 
-// TODO: big refactor of this module. (just testing for now)
-
-// func saveJSON(fileName string, data string) {
-// 	file, err := os.Create(fileName)
-
-// 	if err != nil {
-// 		panic(err)
-// 	}
-
-// 	defer file.Close()
-
-// 	file.WriteString(data)
-// }
-
-func ApiTest() {
-	// fmt.Println("hello from wikiapi")
-
-	w, err := mwclient.New("https://en.wikipedia.org/w/api.php", "wikiPasswordSearch")
+func GetRandArticle() {
+	wiki, err := mwclient.New("https://en.wikipedia.org/w/api.php", "wikiPasswordSearch")
 
 	if err != nil {
-		panic(err)
+		log.Fatalln("[ERROR]: Something went wrong while connecting to the API: ", err)
 	}
 
 	parameters := params.Values{
 		"action":  "query",
 		"format":  "json",
 		"list":    "random",
-		"rnlimit": "5",
+		"rnlimit": "1",
 	}
 
-	response, err := w.Get(parameters)
+	response, err := wiki.Get(parameters)
 
 	if err != nil {
-		panic(err)
+		log.Fatalln("[ERROR]: Invalid API server response: ", err)
 	}
 
-	// data := map[string]string{}
-
 	fmt.Println(response)
-	// var data map[string]interface{}
-	// json.Unmarshal([]byte(response), &data)
-
-	// for _, el := range response.Map() {
-	// 	fmt.Println(*el)
-	// }
-
-	// saveJSON("./pkg/wiki/data.json", data)
 }
