8fe46bf0a6bc71d77a552116bd05085861c6b87b
Paweł Rybak <pawel.rybak13@gmail.com>
2023-05-23 22:40:40 +0200

    Added a path for log/ directory and stdout file

--- pkg/consts/consts.go
+++ pkg/consts/consts.go
@@ -1,7 +1,9 @@
 package consts
 
 const (
+	LogDir         = "./log"
 	SecretDir      = "./secret"
 	EncryptionFile = "./secret/passwd.aes"
 	HashFile       = "./secret/mpasswd.hash"
+	PrintLog       = "/terminal-stdout.log"
 )
