4dbe80af651bc18da5b41378c91ec24a7bf8a820
Paweł Rybak <pawel.rybak13@gmail.com>
2023-05-24 15:59:24 +0200

    Tested key generation

--- wikipass.go
+++ wikipass.go
@@ -22,9 +22,4 @@ func main() {
 
 	message := aeswrapper.DecryptAES(consts.EncryptionFile, []byte(key))
 	fmt.Println(string(message))
-
-	hashMessage := aeswrapper.HashBytes([]byte(plainText))
-	stringHash := aeswrapper.ByteToString(hashMessage[:])
-
-	fmt.Println(stringHash)
 }
