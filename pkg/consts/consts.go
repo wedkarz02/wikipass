a6babe09b57ceb55071d4baa6bc24e4c92bb1f9b
Paweł Rybak <pawel.rybak13@gmail.com>
2023-06-03 22:05:46 +0200

    Very messy text input box - will be cleaned up later

--- pkg/consts/consts.go
+++ pkg/consts/consts.go
@@ -5,3 +5,8 @@ const (
 	EncryptionFile = "./secret/passwd.aes"
 	IVFile         = "./secret/initvec.bin"
 )
+
+const (
+	LogWindowWidth  = 400
+	LogWindowHeight = 550
+)
--- wikipass.go
+++ wikipass.go
@@ -2,46 +2,86 @@ package main
 
 import (
 	"fmt"
-	"sync"
-	"wikipass/pkg/pwder"
+	"strings"
+	c "wikipass/pkg/consts"
 
 	rl "github.com/gen2brain/raylib-go/raylib"
 )
 
 func main() {
-	rl.InitWindow(800, 450, "raylib [core] example - basic window")
+	rl.InitWindow(c.LogWindowWidth, c.LogWindowHeight, "Wikipass")
 	defer rl.CloseWindow()
 	rl.SetTargetFPS(60)
 
-	font := rl.LoadFont("./assets/fonts/JetBrainsMono-Regular.ttf")
+	font := rl.LoadFontEx("./assets/fonts/JetBrainsMono-Bold.ttf", 40, nil)
 
-	var wg sync.WaitGroup
-	var passwords []string
-	passwdChan := make(chan string)
+	bgColor := rl.Color{R: 0x2F, G: 0x36, B: 0x3D, A: 0xFF}
+	textColor := rl.Color{R: 0xD1, G: 0xD5, B: 0xDA, A: 0xFF}
 
-	n := 15
+	text := "Input your\nmaster password:"
+	textSize := rl.MeasureTextEx(font, text, 40, 0)
 
-	for i := 0; i < n; i++ {
-		wg.Add(1)
-		go pwder.GenPassword(passwdChan, &wg)
-	}
+	textX := c.LogWindowWidth/2 - textSize.X/2
+	textY := c.LogWindowHeight/2 - textSize.Y/2 - 80
 
-	for i := 0; i < n; i++ {
-		passwords = append(passwords, <-passwdChan)
-	}
+	var inputText []string
+	var hidden []string
 
-	wg.Wait()
+	textBox := rl.Rectangle{X: c.LogWindowWidth * 0.12,
+		Y:      c.LogWindowHeight/2 - 25,
+		Width:  c.LogWindowWidth * 0.75,
+		Height: 50}
 
-	for _, passwd := range passwords {
-		fmt.Println(passwd)
-	}
+	mouseOnText := false
 
 	for !rl.WindowShouldClose() {
+		if rl.CheckCollisionPointRec(rl.GetMousePosition(), textBox) {
+			mouseOnText = true
+		} else {
+			mouseOnText = false
+		}
+
+		if mouseOnText {
+			rl.SetMouseCursor(rl.MouseCursorIBeam)
+		} else {
+			rl.SetMouseCursor(rl.MouseCursorDefault)
+		}
+
+		key := rl.GetCharPressed()
+
+		for key > 0 {
+			if key > 32 && key < 125 {
+				inputText = append(inputText, string(key))
+				hidden = append(hidden, "*")
+			}
+
+			key = rl.GetCharPressed()
+		}
+
+		if rl.IsKeyPressed(rl.KeyBackspace) {
+			if len(inputText) > 0 {
+				inputText = inputText[:len(inputText)-1]
+				hidden = hidden[:len(hidden)-1]
+			}
+		}
+
 		rl.BeginDrawing()
 
-		rl.ClearBackground(rl.RayWhite)
-		rl.DrawTextEx(font, "Hello, world! In JBMono!", rl.Vector2{X: 130, Y: 200}, 50, 1, rl.LightGray)
+		rl.ClearBackground(bgColor)
+		rl.DrawTextEx(font,
+			text,
+			rl.Vector2{X: textX, Y: textY},
+			40, 0, textColor)
+
+		rl.DrawRectangleRec(textBox, rl.LightGray)
+		rl.DrawRectangleLinesEx(textBox, 1, rl.Black)
+
+		rl.DrawTextEx(font, strings.Join(hidden, ""),
+			rl.Vector2{X: textBox.X + 5, Y: textBox.Y + 8},
+			40, 0, rl.Black)
 
 		rl.EndDrawing()
 	}
+
+	fmt.Println(strings.Join(inputText, ""))
 }
