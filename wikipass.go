86232a8587ef9f3838d35e609c3264962cced064
Paweł Rybak <pawel.rybak13@gmail.com>
2023-06-03 11:09:26 +0200

    Test window in raylib

--- wikipass.go
+++ wikipass.go
@@ -1,50 +1,18 @@
 package main
 
-import (
-	"github.com/veandco/go-sdl2/sdl"
-)
+import rl "github.com/gen2brain/raylib-go/raylib"
 
 func main() {
-	if err := sdl.Init(sdl.INIT_EVERYTHING); err != nil {
-		panic(err)
-	}
+	rl.InitWindow(800, 450, "raylib [core] example - basic window")
+	defer rl.CloseWindow()
+	rl.SetTargetFPS(60)
 
-	defer sdl.Quit()
+	for !rl.WindowShouldClose() {
+		rl.BeginDrawing()
 
-	window, err := sdl.CreateWindow("test",
-		sdl.WINDOWPOS_UNDEFINED,
-		sdl.WINDOWPOS_UNDEFINED,
-		800, 600,
-		sdl.WINDOW_SHOWN)
+		rl.ClearBackground(rl.RayWhite)
+		rl.DrawText("Congrats! You created your first window!", 190, 200, 20, rl.LightGray)
 
-	if err != nil {
-		panic(err)
-	}
-
-	defer window.Destroy()
-
-	surface, err := window.GetSurface()
-
-	if err != nil {
-		panic(err)
-	}
-
-	surface.FillRect(nil, 0)
-
-	rect := sdl.Rect{X: 0, Y: 0, W: 200, H: 200}
-	color := sdl.Color{R: 0, G: 255, B: 0, A: 255}
-	pixel := sdl.MapRGBA(surface.Format, color.R, color.G, color.B, color.A)
-	surface.FillRect(&rect, pixel)
-
-	window.UpdateSurface()
-
-	running := true
-	for running {
-		for event := sdl.PollEvent(); event != nil; event = sdl.PollEvent() {
-			switch event.(type) {
-			case *sdl.QuitEvent:
-				running = false
-			}
-		}
+		rl.EndDrawing()
 	}
 }
